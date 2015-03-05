66d2a05da9bbe901596e039dda21de3ede8b514e
amassiro <massironi.andrea@gmail.com>
2015-03-05 20:29:42 +0100

    up

--- Plot/DrawNjet2.cxx
+++ Plot/DrawNjet2.cxx
@@ -1,76 +1,58 @@
 {
  //---- VBF central jet veto
  
- TH1F* h_qqH_norm = new TH1F ("qqH_norm","qqH",2, 0, 2);
+//  latino->Draw("1"," baseW * effW * triggW * ( (njet>=2 && njet<=3 && (jetpt3<=30 || !(jetpt3 > 30 && ( (jeteta1-jeteta3 > 0 && jeteta2-jeteta3 < 0) || (jeteta2-jeteta3 > 0 && jeteta1-jeteta3 < 0)))))       &&     (ch1*ch2 ==-1) && trigger==1. && pt1>20 && pt2>20 && mll>70. && (nextra==0) * (bveto_mu && bveto_ip && nbjettche==0) && mjj>500 && ptll>45 && njet>=2 && abs(eta1 - (jeteta1+jeteta2)/2)/detajj < 0.5 && abs(eta2 - (jeteta1+jeteta2)/2)/detajj < 0.5 && detajj>2.5)")
+//  latino->Draw("1"," baseW * effW * triggW * (                                                                                                                                                                        (ch1*ch2 ==-1) && trigger==1. && pt1>20 && pt2>20 && mll>70. && (nextra==0) * (bveto_mu && bveto_ip && nbjettche==0) && mjj>500 && ptll>45 && njet>=2 && abs(eta1 - (jeteta1+jeteta2)/2)/detajj < 0.5 && abs(eta2 - (jeteta1+jeteta2)/2)/detajj < 0.5 && detajj>2.5)")
+ 
+ 
+ 
+ 
+ TH1F* h_powheg_norm = new TH1F ("powheg_norm","powheg",2, 0, 2);
  TH1F* h_ggH_norm = new TH1F ("ggH_norm","ggH",2, 0, 2);
 
  
- TH1F* h_qqH = new TH1F ("qqH","qqH",2, 0, 2);
+ TH1F* h_powheg = new TH1F ("powheg","powheg",2, 0, 2);
  TH1F* h_ggH = new TH1F ("ggH","ggH",2, 0, 2);
  
- //---- 0 jet
- h_qqH -> SetBinContent(1,3.1310);
- h_ggH -> SetBinContent(1,247.0227);
- h_qqH_norm -> SetBinContent(1,3.1310/(3.1310+247.0227));
- h_ggH_norm -> SetBinContent(1,247.0227/(3.1310+247.0227));
+ //---- 0 = cjv, 1 = !cjv
+ h_powheg -> SetBinContent(1,5.494381e-01);
+ h_powheg -> SetBinContent(2,5.767007e-01-5.494381e-01);
  
- //---- 1 jet
- h_qqH -> SetBinContent(2, 8.7510);
- h_ggH -> SetBinContent(2, 69.8991);
- h_qqH_norm -> SetBinContent(2, 8.7510/(8.7510+69.8991));
- h_ggH_norm -> SetBinContent(2, 69.8991/(8.7510+69.8991));
- 
- 
- h_qqH->SetFillColor(kBlue);
- h_qqH->SetLineColor(kBlue);
- h_qqH->SetFillStyle(3004);
+ h_powheg->SetFillColor(kBlue);
+ h_powheg->SetLineColor(kBlue);
+ h_powheg->SetFillStyle(3004);
  
  h_ggH->SetFillColor(kRed);
  h_ggH->SetLineColor(kRed);
  h_ggH->SetFillStyle(3003);
 
-
-
- h_qqH_norm->SetFillColor(kBlue);
- h_qqH_norm->SetLineColor(kBlue);
- h_qqH_norm->SetFillStyle(3004);
  
  h_ggH_norm->SetFillColor(kRed);
  h_ggH_norm->SetLineColor(kRed);
  h_ggH_norm->SetFillStyle(3003);
 
-
+ h_powheg_norm
 
  
  TLegend * legend = new TLegend(0.83,0.93,0.95,0.20);
- legend -> AddEntry(h_ggH, "ggH");
- legend -> AddEntry(h_qqH, "qqH");
+//  legend -> AddEntry(h_ggH, "ggH");
+ legend -> AddEntry(h_powheg, "powheg");
  legend -> SetFillColor(kWhite);
  
- THStack* hs = new THStack("st","");
- hs->Add(h_qqH);
- hs->Add(h_ggH);
  
  TCanvas* cc = new TCanvas("cComposition","cComposition",1000,500);
-//  hs->Draw("C");
- hs->Draw("histo");
- hs->GetXaxis()->SetTitle("central jet veto");
- hs->GetXaxis()->SetRangeUser(110,600);
- hs->GetYaxis()->SetRangeUser(0,1);
- hs->GetYaxis()->SetTitle("Expected events");
+ h_powheg->Draw();
+ h_powheg->GetXaxis()->SetTitle("central jet veto");
+ h_powheg->GetYaxis()->SetTitle("Expected events");
  legend->Draw();
  gPad->SetGrid();
  
- THStack* hs_norm = new THStack("st_norm","");
- hs_norm->Add(h_qqH_norm);
- hs_norm->Add(h_ggH_norm);
  
  TCanvas* cc_norm = new TCanvas("cCompositionNorm","cCompositionNorm",1000,500);
- hs_norm->Draw("histo");
- hs_norm->GetXaxis()->SetTitle("central jet veto");
- hs_norm->GetXaxis()->SetRangeUser(110,600);
- hs_norm->GetYaxis()->SetRangeUser(0,1);
- hs_norm->GetYaxis()->SetTitle("relative composition");
+ h_powheg->DrawNormalized();
+ h_powheg->GetXaxis()->SetTitle("central jet veto");
+ h_powheg->GetYaxis()->SetRangeUser(0,1);
+ h_powheg->GetYaxis()->SetTitle("relative composition");
  legend->Draw();
  gPad->SetGrid();
  
