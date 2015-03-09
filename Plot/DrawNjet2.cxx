cc6edcc7ce0585575c152f721f72333642684cf2
amassiro <massironi.andrea@gmail.com>
2015-03-09 22:59:20 +0100

    up

--- Plot/DrawNjet2.cxx
+++ Plot/DrawNjet2.cxx
@@ -4,11 +4,18 @@
 //  r99t ../../../output_Latinos_tree_2012_2orMoreJet2012_wwewk/latino_2500_vbfToH500toWWTo2LAndTau2Nu.root
 //  latino->Draw("1"," baseW * effW * triggW * ( (njet>=2 && njet<=3 && (jetpt3<=30 || !(jetpt3 > 30 && ( (jeteta1-jeteta3 > 0 && jeteta2-jeteta3 < 0) || (jeteta2-jeteta3 > 0 && jeteta1-jeteta3 < 0)))))       &&     (ch1*ch2 ==-1) && trigger==1. && pt1>20 && pt2>20 && mll>70. && (nextra==0) * (bveto_mu && bveto_ip && nbjettche==0) && mjj>500 && ptll>45 && njet>=2 && abs(eta1 - (jeteta1+jeteta2)/2)/detajj < 0.5 && abs(eta2 - (jeteta1+jeteta2)/2)/detajj < 0.5 && detajj>2.5)")
 //  latino->Draw("1"," baseW * effW * triggW * (                                                                                                                                                                        (ch1*ch2 ==-1) && trigger==1. && pt1>20 && pt2>20 && mll>70. && (nextra==0) * (bveto_mu && bveto_ip && nbjettche==0) && mjj>500 && ptll>45 && njet>=2 && abs(eta1 - (jeteta1+jeteta2)/2)/detajj < 0.5 && abs(eta2 - (jeteta1+jeteta2)/2)/detajj < 0.5 && detajj>2.5)")
-
+// 
+//  latino->Draw("1", "baseW * effW * triggW * (mWW>130)*( 0.125*( (dataset == 160) || (dataset == 169) || (dataset == 176) || (dataset == 172)) - 0.250*( (dataset == 161) || (dataset == 175) || (dataset == 173) || (dataset == 170)) + 0.125*( (dataset == 162) || (dataset == 171) || (dataset == 174) || (dataset == 177)))")
  
- TH1F* h_powheg_norm = new TH1F ("powheg_norm","powheg",2, 0, 2);
+//  1.21975220739841461e-01
+//  3.49249510722408119e-01
  
+ TH1F* h_powheg_norm = new TH1F ("powheg_norm","powheg",2, 0, 2); 
  TH1F* h_powheg = new TH1F ("powheg","powheg",2, 0, 2);
+
+ TH1F* h_phantom_norm = new TH1F ("phantom_norm","phantom",2, 0, 2); 
+ TH1F* h_phantom = new TH1F ("phantom","phantom",2, 0, 2);
+
  
  //---- 0 = cjv, 1 = !cjv
  h_powheg -> SetBinContent(1,5.494381e-01);
@@ -18,19 +25,36 @@
  h_powheg->SetLineColor(kBlue);
  h_powheg->SetFillStyle(3004);
  
+ h_powheg->Sumw2();
+ 
  h_powheg_norm = (TH1F*) h_powheg->Clone();
  float integral = h_powheg_norm->Integral();
  h_powheg_norm -> Scale (1. / integral);
 
  
+ h_phantom -> SetBinContent(1,5.494381e-01);
+ h_phantom -> SetBinContent(2,5.767007e-01-5.494381e-01);
+ 
+ h_phantom->SetFillColor(kRed);
+ h_phantom->SetLineColor(kRed);
+ h_phantom->SetFillStyle(3004);
+ 
+ h_phantom->Sumw2();
+ 
+ h_phantom_norm = (TH1F*) h_phantom->Clone();
+ integral = h_phantom_norm->Integral();
+ h_phantom_norm -> Scale (1. / integral);
+ 
+ 
  TLegend * legend = new TLegend(0.70,0.90,0.90,0.70);
-//  legend -> AddEntry(h_ggH, "ggH");
  legend -> AddEntry(h_powheg, "powheg");
+ legend -> AddEntry(h_phantom, "phantom");
  legend -> SetFillColor(kWhite);
  
  
  TCanvas* cc = new TCanvas("cComposition","cComposition",1000,500);
- h_powheg->Draw();
+ h_powheg->Draw("E");
+ h_phantom->Draw("Esame");
  h_powheg->GetXaxis()->SetTitle("central jet veto");
  h_powheg->GetYaxis()->SetTitle("Expected events");
  legend->Draw();
@@ -38,7 +62,8 @@
  
  
  TCanvas* cc_norm = new TCanvas("cCompositionNorm","cCompositionNorm",1000,500);
- h_powheg_norm->Draw();
+ h_powheg_norm->Draw("E");
+ h_phantom_norm->Draw("Esame");
  h_powheg_norm->GetXaxis()->SetTitle("central jet veto");
  h_powheg_norm->GetYaxis()->SetRangeUser(0,1);
  h_powheg_norm->GetYaxis()->SetTitle("relative composition");
